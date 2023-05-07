e4e683a4fb6cb96be95a52140a99e8906035e1eb
aga87 <aga.labonarska@outlook.com>
2023-05-07 16:05:16 +0200

    Extract navigation as a feature

---
+++ src/features/auth/components/SignOut/useSignOut.ts
@@ -0,0 +1,12 @@
+import { useAppDispatch } from '@/redux/typed-hooks';
+import { signOut } from '../../redux/authSlice';
+
+export const useSignOut = () => {
+  const dispatch = useAppDispatch();
+
+  const handleSignOutClick = () => {
+    dispatch(signOut());
+  };
+
+  return { handleSignOutClick };
+};
--- src/features/auth/components/index.ts
+++ src/features/auth/components/index.ts
@@ -2,5 +2,5 @@ export * from './AuthRedirect/AuthRedirect';
 export * from './SignInForm/SignInForm';
 export * from './SignInWithGitHub/SignInWithGitHub';
 export * from './SignInWithGoogle/SignInWithGoogle';
-export * from './SignOut/SignOut';
+export * from './SignOut/useSignOut';
 export * from './SignUpForm/SignUpForm';
