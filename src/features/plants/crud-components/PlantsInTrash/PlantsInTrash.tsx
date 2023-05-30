cfc2f341da8af3e589e51d3d3f47231164edf698
aga87 <aga.labonarska@outlook.com>
2023-05-30 19:48:50 +0200

    Implement the UI for restoring plants from trash

--- src/features/plants/crud-components/PlantsInTrash/PlantsInTrash.tsx
+++ src/features/plants/crud-components/PlantsInTrash/PlantsInTrash.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import { Alert, Heading, Logo } from '@/components';
+import { useAppDispatch } from '@/redux/typed-hooks';
+import { restorePlant } from '../../redux/plants/thunks';
+import { Alert, Heading, Logo, RestoreIcon } from '@/components';
 import { PlantDescription, PlantChart } from '../../components';
 import { useFetchPlantsInTrash } from './useFetchPlantsInTrash';
+import type { Plant } from '../../types';
 import styles from './plants-in-trash.module.scss';
 
 export const PlantsInTrash = () => {
   const { plantsInTrash, isLoading, error } = useFetchPlantsInTrash();
 
+  const dispatch = useAppDispatch();
+
+  const handleRestoreClick = (plantId: Plant['_id']) => {
+    dispatch(restorePlant(plantId));
+  };
+
   const plantListItems = plantsInTrash.map(plant => {
     return (
-      <li key={plant._id} className={styles.list__item}>
-        <PlantDescription
-          name={plant.name}
-          variety={plant.variety}
-          category={plant.category}
-        />
-        <PlantChart
-          sowFrom={plant.sowFrom}
-          sowUntil={plant.sowUntil}
-          harvestFrom={plant.harvestFrom}
-          harvestUntil={plant.harvestUntil}
-        />
+      <li key={plant._id} className={styles.listItem}>
+        <div className={styles.listItem__plant}>
+          <PlantDescription
+            name={plant.name}
+            variety={plant.variety}
+            category={plant.category}
+          />
+          <PlantChart
+            sowFrom={plant.sowFrom}
+            sowUntil={plant.sowUntil}
+            harvestFrom={plant.harvestFrom}
+            harvestUntil={plant.harvestUntil}
+          />
+        </div>
+        <button
+          className={styles.listItem__button}
+          type='button'
+          onClick={() => handleRestoreClick(plant._id)}
+        >
+          <RestoreIcon />
+          Restore
+        </button>
       </li>
     );
   });
